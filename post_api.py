cec0d6316b3e2e71884537eb0edd86b34da0b8ca
muhdamrullah <muneorion@gmail.com>
2016-12-20 02:24:50 +0800

    Update post_api.py

--- post_api.py
+++ post_api.py
@@ -4,6 +4,7 @@ import csv
 import json
 import requests
 import pprint
+import time
 
 airodump_csv_to_json.convert()
 print(len(glob.glob1('./',"*kismet.csv")))
@@ -13,5 +14,10 @@ with open('dump.json') as data_file:
 
 headers = {'Content-type': 'application/json; charset=utf-8'}
 payload = {'status':'OK', 'mobile_signal':data}
-r = requests.post('http://localhost:3001/mobile_signal/', data=json.dumps(payload), headers=headers)
-print r.status_code
+try:
+    r = requests.post('http://localhost:3001/mobile_signal/', data=json.dumps(payload), headers=headers)
+    print r.status_code
+except requests.exceptions.ConnectionError as e:
+    print e
+    r = "No response"
+    print r
